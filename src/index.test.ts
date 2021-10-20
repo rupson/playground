06014c3999c4135a5b8709aa16b5f37e0c4298c0
Rob Upson <rupson1998@gmail.com>
2021-10-20 09:24:52 +0100

    more generic

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,60 @@
-import { myFunction } from "."
+import { Input, transformCsvData } from "."
 
-describe('it does something', () => {
-  it('says hello', () => {
-    expect(myFunction()).toEqual('hello world')
+const csv: Input[] = [{
+        a: '1234',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        e: 'e',
+        f: 'f',
+        g: 'g',
+        h: 'h'
+    },
+    {
+        a: '1234',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        e: 'e',
+        f: 'f',
+        g: 'g',
+        h: 'h'
+    },
+    {
+        a: 'abcd',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        e: 'e',
+        f: 'f',
+        g: 'g',
+        h: 'h'
+    }
+]
+
+const expected = [{
+        a: '1234',
+        b: 'b',
+        c: 'c',
+        d: ['d','d'],
+        e: ['e','e'],
+        f: ['f','f'],
+        g: ['g','g'],
+        h: ['h','h']
+    },{
+        a: 'abcd',
+        b: 'b',
+        c: 'c',
+        d: ['d'],
+        e: ['e'],
+        f: ['f'],
+        g: ['g'],
+        h: ['h']
+    }]
+
+
+describe('transformCsvData', () => {
+  it('groups properly', () => {
+    expect(transformCsvData('a')(csv)).toMatchObject(expected)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-export const myFunction = () => 'hello world'
+// @ts-nocheck
+
+import * as R from "ramda";
+
+export type Input = Record<string, string>
+
+const combineOnlyArrays = R.cond([
+  [Array.isArray, R.concat],
+  [R.T, R.identity],
+]);
+
+const combineObjects = (arr) =>
+  arr.reduce((acc, curr) => R.mergeWith(combineOnlyArrays, acc, curr), {});
+
+const makeArray = (x) => [x];
+
+const isConcatableKey = (key) =>
+  !["a", "b", "c"].includes(key);
+
+const mapperByKey = (value, key) =>
+  isConcatableKey(key) ? makeArray(value) : value;
+
+const transformRow = R.mapObjIndexed(mapperByKey);
+
+export const transformCsvData = (groupByPropName: string) => (csv: Input[]) => {
+  return R.pipe(
+    R.map(transformRow),
+    R.groupBy(R.prop(groupByPropName)),
+    R.map(combineObjects),
+    Object.values
+  )(csv);
+};
