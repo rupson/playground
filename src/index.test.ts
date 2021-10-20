c4fd763efe7fdd13530169063e08c206dab83396
Rob Upson <rupson1998@gmail.com>
2021-10-20 13:56:41 +0100

    repeatWith fn

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,18 @@
-import { repeat } from ".";
+import { repeat, repeatWith } from ".";
 
 describe("repeat", () => {
   it("repeats a variable", () => {
     expect(repeat("x")(5)).toMatchObject(["x", "x", "x", "x", "x"]);
+  });
+});
+describe("repeatWith", () => {
+  it("repeats a variable with a key", () => {
+    expect(repeatWith("x")((elt, index) => `${elt}${index}`)(5)).toMatchObject([
+      "x0",
+      "x1",
+      "x2",
+      "x3",
+      "x4",
+    ]);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-export const repeat = <T>(repetitor: T) => (length: number) =>
-  [...new Array(length)].map(() => repetitor);
+export const repeat =
+  <T>(repetitor: T) =>
+  (length: number) =>
+    [...new Array(length)].map(() => repetitor);
 
-
-// type RepeatFunctor<T> = ((input: T) => unknown) | ((input: T, index: number) => unknown)
-// export const repeatWith = <T>(repetitor: T) => (functor: (arg: RepeatFunctor<T>) => unknown) => (length: number) =>
-//   [...new Array(length)].map((_, index) => functor(repetitor, index));
+type RepeatFunctor<T> = (input: T, index: number) => unknown;
+export const repeatWith =
+  <T>(repetitor: T) =>
+  (functor: RepeatFunctor<T>) =>
+  (length: number) =>
+    [...new Array(length)].map((_, index) => functor(repetitor, index));
