6becf66f77932fe05e54f593538f5050ef0ba24f
Rob Upson <rupson1998@gmail.com>
2021-10-26 14:41:11 +0100

    infinite

---
+++ typescript/src/lampWithInfiniteRecursion.ts
@@ -0,0 +1,17 @@
+const on = () => {
+  console.log('on')
+  setTimeout(() => {
+    off()
+  }, 0)
+}
+
+const off = () => {
+  console.log('off')
+  setTimeout(() => {
+    on()
+  }, 0)
+}
+
+const lamp = () => on();
+
+lamp()
